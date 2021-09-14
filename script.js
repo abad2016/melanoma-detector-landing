ae3bbbb0c1718c42f32d429490269df9b1d72b6d
abad2016 <57024664+abad2016@users.noreply.github.com>
2021-09-14 19:39:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.card',{
+  duration:2000,
+  origin: 'bottom',
+  distance: '20px',
+})
+
+sr.reveal('.imagen',{
+  duration:2000,
+  origin: 'right',
+  distance: '200px',
+})
+
+sr.reveal('.btn-lg',{
+  duration:2000,
+  origin: 'left',
+  distance: '100px',
+})
+
+
+
+// Smooth scrolling using jQuery easing
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+        location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+    ) {
+        var target = $(this.hash);
+        target = target.length
+            ? target
+            : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+            $("html, body").animate(
+                {
+                    scrollTop: target.offset().top + 1,
+                },
+                1000,
+                "easeInOutExpo"
+            );
+            return false;
+        }
+    }
+});
+
+   // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
